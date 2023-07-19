a19a40cb962b629773febe20c3dd76733af82096
Udiomuno Ezekiel <geniuswealthb@gmail.com>
2023-07-19 22:50:18 +0100

    Add validations for menu_items and category modela

--- app/models/menu_item.rb
+++ app/models/menu_item.rb
@@ -1,2 +1,15 @@
 class MenuItem < ApplicationRecord
+  validates :name, presence: true
+  validates :price, presence: true
+  validates :description, presence: true
+  validates :category_id, presence: true
+  validates :sub_category_id, presence: true
+  validates :restaurant_id, presence: true
+
+  belongs_to :category
+  belongs_to :sub_category
+  belongs_to :restaurant
+
+  has_many :order
+
 end
