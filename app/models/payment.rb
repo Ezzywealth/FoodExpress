ac3a3def353161d6bf77662ddeb26161c044c442
Udiomuno Ezekiel <geniuswealthb@gmail.com>
2023-07-22 18:43:47 +0100

    Created db

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,2 +1,6 @@
 class Payment < ApplicationRecord
+  validates :restaurant_id, presence: true
+  validates :payment_status, presence: true
+  validates :total_amount, presence: true
+  validates :customer_id, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -83,6 +83,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_19_042305) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
