f3ac20ba8ab15aacc266fd465c016179d13fa9c2
Udiomuno Ezekiel <geniuswealthb@gmail.com>
2023-07-26 17:58:06 +0100

    Chanegs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,14 @@
 class ApplicationController < ActionController::API
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def current_user
+    if request.headers['Authorization'].present?
+      jwt_payload = JWT.decode(request.headers['Authorization'].split.last,
+                               Rails.application.credentials.devise_jwt_secret_key!).first
+      current_user = User.find(jwt_payload['sub'])
+    end
+  end
+
   protected
 
   def configure_permitted_parameters
--- db/migrate/20230725081347_add_name_to_user.rb
+++
@@ -1,7 +0,0 @@
-class AddNameToUser < ActiveRecord::Migration[7.0]
-  def change
-    add_column :users, :name, :string, :null => false
-    #Ex:- :null => false
-    #Ex:- add_column("admin_users", "username", :string, :limit =>25, :after => "email")
-  end
-end
