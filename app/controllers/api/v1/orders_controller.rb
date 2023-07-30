f06e6526fe98b90c4eb32a78cfb8b2a06b86319b
Samuel tobby <sarhmullah15@gmail.com>
2023-07-30 10:40:12 -0700

    Create menu_orders and restaurant_orders

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -13,7 +13,32 @@ module Api
       # #create order
       order = Order.new(customer_id: customer.id,total_amount: 0, fulfilled: false, payment_id: payment.id)
       order.save
-     
+       
+      #loop through the array from frontend
+      orders = orders_params[:menu_ids]
+      orders.each do |order_params|
+        # Create menu_order
+        menu = MenuItem.where(id: order_params[:menu_id]).first
+          menu_order = MenuOrder.new(
+          menuItem_id: menu.id,
+          order_id: order.id,
+          quantity: order_params[:quantity],
+          total_price: order_params[:total_price]
+        )
+        menu_order.save
+        
+        # Create restaurant_order
+        restaurant = menu.restaurant
+        res_order = RestaurantOrder.new(
+          customer_id: customer.id,
+          menuItem_id: menu.id,
+          restaurant_id: restaurant.id,
+          payment_id: payment.id,
+          quantity: order_params[:quantity],
+          price: order_params[:total_price]
+        )
+        res_order.save
+       end
 
       private
       def orders_params
