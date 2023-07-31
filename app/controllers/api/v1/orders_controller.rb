0b1ce6e221ee1cbe2434aa56284cad49d69ed3e5
Samuel tobby <sarhmullah15@gmail.com>
2023-07-31 03:59:13 -0700

    remove all comments

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -7,22 +7,24 @@ module Api
       end
 
       def create
+        ## find the customer if it exist
         customer = Customer.find_by(id: orders_params[:customer_id])
         if customer.nil?
           render json: { error: 'Customer not found' }, status: :unprocessable_entity
           return
         end
-
+        # create the payment id
         payment = Payment.new(customer_id: customer.id, total_amount: 0, fulfilled: false)
         payment.save
-
+        # create the order id
         order = Order.new(customer_id: customer.id, total_amount: 0, fulfilled: false, payment_id: payment.id)
         order.save
-
+        # loop through the array of food order from frontend
         orders_params[:menu_ids].each do |order_params|
           menu = MenuItem.find_by(id: order_params[:menu_id])
           next unless menu
 
+          # create menu order
           menu_order = MenuOrder.new(
             menuItem_id: menu.id,
             order_id: order.id,
@@ -30,7 +32,7 @@ module Api
             total_price: order_params[:total_price]
           )
           menu_order.save
-
+          # Create restaurant order
           restaurant = menu.restaurant
           res_order = RestaurantOrder.new(
             customer_id: customer.id,
@@ -42,7 +44,7 @@ module Api
           )
           res_order.save
         end
-
+        # return payment data and order data
         render json: { payment:, order: }, status: :ok
       end
 
@@ -54,55 +56,3 @@ module Api
     end
   end
 end
-
-
-
-# module Api
-#   module V1
-#     class OrdersController < ApplicationController
-#       def create
-#         customer = Customer.where(id: orders_params[:customer_id]).first
-
-#          render json: [payment, order], status: :ok
-#       end
-#       #create payment
-#       payment = Payment.new(customer_id: customer.id, total_amount: 0, fulfilled: false)
-#       payment.save
-
-#       # #create order
-#       order = Order.new(customer_id: customer.id,total_amount: 0, fulfilled: false, payment_id: payment.id)
-#       order.save
-
-#       #loop through the array from frontend
-#       orders = orders_params[:menu_ids]
-#       orders.each do |order_params|
-#         # Create menu_order
-#         menu = MenuItem.where(id: order_params[:menu_id]).first
-#           menu_order = MenuOrder.new(
-#           menuItem_id: menu.id,
-#           order_id: order.id,
-#           quantity: order_params[:quantity],
-#           total_price: order_params[:total_price]
-#         )
-#         menu_order.save
-
-#         # Create restaurant_order
-#         restaurant = menu.restaurant
-#         res_order = RestaurantOrder.new(
-#           customer_id: customer.id,
-#           menuItem_id: menu.id,
-#           restaurant_id: restaurant.id,
-#           payment_id: payment.id,
-#           quantity: order_params[:quantity],
-#           price: order_params[:total_price]
-#         )
-#         res_order.save
-#        end
-
-#       private
-#       def orders_params
-#           params.require(:order).permit(:customer_id, menu_ids: [:menu_id, :quantity, :total_price])
-#       end
-#    end
-#   end
-# end
